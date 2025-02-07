3feacc2c68f25ee0a6eacb93fa653d8cd1b92eb0
JairoECC <jairocalderon680@gmail.com>
2025-02-07 15:04:21 -0500

    solucion de error

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'https://i.ibb.co/njPcJ5b/JC-logo.png',
 
   // Set the production url of your site here
-  url: 'https://jairoecc.github.io/',
+  url: 'https://miformulario.xyz',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/caso7/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
